c795c63b448a8bb459589037f0581afcefb19d35
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-02-11 15:15:26 +0000

    AD0001 Replication

---
+++ AD0001.Replication/Tests.cs
@@ -0,0 +1,12 @@
+﻿using TUnit.Core;
+
+namespace AD0001.Replication;
+
+public class Tests
+{
+    [DataDrivenTest(null)]
+    public async Task Test(string? value)
+    {
+        await Task.CompletedTask;
+    }
+}
